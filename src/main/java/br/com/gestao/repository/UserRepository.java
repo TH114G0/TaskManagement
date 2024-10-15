94246c4e4d5a5d1765ac3667990a5ba97dd2d8fd
Thiago Jose <thiago.wilton2704@gmail.com>
2024-10-15 15:05:23 -0300

    Commit: Adiciona o repositório UserRepository Implementa a interface UserRepository, que estende JpaRepository para a entidade UserModel. Esta interface fornece operações padrão de CRUD (Create, Read, Update, Delete) para gerenciar usuários no sistema, facilitando o acesso e manipulação dos dados de usuários no banco de dados.

---
+++ src/main/java/br/com/gestao/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package br.com.gestao.repository;
+
+import br.com.gestao.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {}
